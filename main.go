5845f482a047da1fcae3b1e5134c48b248b72ff5
Xaidel <abechuelak@gmail.com>
2025-08-10 17:18:37 +0800

    init

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+	"server/config"
+	"server/lib"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	config.Load()
+	lib.ConnectDatabase()
+}
+
+func main() {
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://192.168.123.39", "http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+	}))
+	router.NoRoute(func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"message": "JSSL"})
+	})
+	router.Run()
+}
