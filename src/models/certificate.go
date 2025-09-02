8fe8285b7ace46c5e61c05ecea7227dd5314781f
jptaycs <125124706+jptaycs@users.noreply.github.com>
2025-09-02 15:18:12 +0800

    certificate

--- src/models/certificate.go
+++ src/models/certificate.go
@@ -1,13 +1,14 @@
 package models
 
-import "time"
-
 type Certificate struct {
-	Resident   Resident
-	ResidentID uint      `gorm:"not null"`
-	Type       string    `gorm:"not null"`
-	Amount     float64   `gorm:"not null"`
-	IssuedDate time.Time `gorm:"type:date;not null"`
-	Ownership  *string
-	ID         uint
+	ID          uint     `json:"id"`
+	Resident    Resident `json:"resident"`
+	ResidentID  uint     `json:"resident_id" gorm:"not null"`
+	Type        string   `json:"type_" gorm:"not null"`
+	Amount      float64  `json:"amount" gorm:"not null"`
+	IssuedDate  string   `json:"issued_date" gorm:"type:date;not null"`
+	Ownership   *string  `json:"ownership_text,omitempty"`
+	CivilStatus string   `json:"civil_status,omitempty"`
+	Purpose     string   `json:"purpose,omitempty"`
+	Age         *int     `json:"age,omitempty"`
 }
